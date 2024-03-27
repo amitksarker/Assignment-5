eb46f78cf13a2f339eb6a87e29e5cf4934a4687e
amitksarker <amitsarker242@gmail.com>
2024-03-27 18:19:53 +0600

    create sit selection background change color function

--- scripts/index.js
+++ scripts/index.js
@@ -6,95 +6,15 @@
  //   const Successsection = document.getElementById('Success-section');
  //   Successsection.classList.remove('hidden');
 
-
-
-
-
-
  function next(elementId){
     hideElementById('home-section');
     showElementById('Success-section');
  }
+// Sit Background Color function
 
 function sit1(elementId){
     hideColorById('A1');
     showColorById('A1');
-    
-    
-    
-    hideColorById('B1');
-    showColorById('B1');
-    hideColorById('B2');
-    showColorById('B2');
-    hideColorById('B3');
-    showColorById('B3');
-    hideColorById('B4');
-    showColorById('B4');
-    hideColorById('C1');
-    showColorById('C1');
-    hideColorById('C2');
-    showColorById('C2');
-    hideColorById('C3');
-    showColorById('C3');
-    hideColorById('C4');
-    showColorById('C4');
-    hideColorById('D1');
-    showColorById('D1');
-    hideColorById('D2');
-    showColorById('D2');
-    hideColorById('D3');
-    showColorById('D3');
-    hideColorById('D4');
-    showColorById('D4');
-    hideColorById('E1');
-    showColorById('E1');
-    hideColorById('E2');
-    showColorById('E2');
-    hideColorById('E3');
-    showColorById('E3');
-    hideColorById('E4');
-    showColorById('E4');
-    hideColorById('F1');
-    showColorById('F1');
-    hideColorById('F2');
-    showColorById('F2');
-    hideColorById('F3');
-    showColorById('F3');
-    hideColorById('F4');
-    showColorById('F4');
-    hideColorById('G1');
-    showColorById('G1');
-    hideColorById('G2');
-    showColorById('G2');
-    hideColorById('G3');
-    showColorById('G3');
-    hideColorById('G4');
-    showColorById('G4');
-    hideColorById('H1');
-    showColorById('H1');
-    hideColorById('H2');
-    showColorById('H2');
-    hideColorById('H3');
-    showColorById('H3');
-    hideColorById('H4');
-    showColorById('H4');
-    hideColorById('I1');
-    showColorById('I1');
-    hideColorById('I2');
-    showColorById('I2');
-    hideColorById('I3');
-    showColorById('I3');
-    hideColorById('I4');
-    showColorById('I4');
-    hideColorById('J1');
-    showColorById('J1');
-    hideColorById('J2');
-    showColorById('J2');
-    hideColorById('J3');
-    showColorById('J3');
-    hideColorById('J4');
-    showColorById('J4');
-
 }
 
 function sit2(elementId){
@@ -112,7 +32,148 @@ function sit4(elementId){
    showColorById('A4');
 }
 
+function sit5(elementId){
+   hideColorById('B1');
+   showColorById('B1');
+}
 
-
-
-
+function sit6(elementId){
+   hideColorById('B2');
+   showColorById('B2');
+}
+function sit7(elementId){
+   hideColorById('B3');
+   showColorById('B3');
+}
+function sit8(elementId){
+   hideColorById('B4');
+   showColorById('B4');  
+}
+function sit9(elementId){
+   hideColorById('C1');
+   showColorById('C1');
+}
+function sit10(elementId){
+   hideColorById('C2');
+   showColorById('C2');
+}
+function sit11(elementId){
+   hideColorById('C3');
+   showColorById('C3');
+}
+function sit12(elementId){
+   hideColorById('C4');
+   showColorById('C4');
+}
+function sit13(elementId){
+   hideColorById('D1');
+   showColorById('D1');
+}
+function sit14(elementId){
+   hideColorById('D2');
+   showColorById('D2');
+}
+function sit15(elementId){
+   hideColorById('D3');
+   showColorById('D3');
+}
+function sit16(elementId){
+   hideColorById('D4');
+   showColorById('D4');
+}
+function sit17(elementId){
+   hideColorById('E1');
+   showColorById('E1');
+}
+function sit18(elementId){
+   hideColorById('E2');
+   showColorById('E2');
+}
+function sit19(elementId){
+   hideColorById('E3');
+   showColorById('E3');
+}
+function sit20(elementId){
+   hideColorById('E4');
+   showColorById('E4');
+}
+function sit21(elementId){
+   hideColorById('F1');
+   showColorById('F1');
+}
+function sit22(elementId){
+   hideColorById('F2');
+   showColorById('F2');
+}
+function sit23(elementId){
+   hideColorById('F3');
+   showColorById('F3');
+}
+function sit24(elementId){
+   hideColorById('F4');
+   showColorById('F4');
+}
+function sit25(elementId){
+   hideColorById('G1');
+   showColorById('G1');
+}
+function sit26(elementId){
+   hideColorById('G2');
+   showColorById('G2');
+}
+function sit27(elementId){
+   hideColorById('G3');
+   showColorById('G3');
+}
+function sit28(elementId){
+   hideColorById('G4');
+   showColorById('G4');
+}
+function sit29(elementId){
+   hideColorById('H1');
+   showColorById('H1');
+}
+function sit30(elementId){
+   hideColorById('H2');
+   showColorById('H2');
+}
+function sit31(elementId){
+   hideColorById('H3');
+   showColorById('H3');
+}
+function sit32(elementId){
+   hideColorById('H4');
+   showColorById('H4');
+}
+function sit33(elementId){
+   hideColorById('I1');
+   showColorById('I1');
+}
+function sit34(elementId){
+   hideColorById('I2');
+   showColorById('I2');
+}
+function sit35(elementId){
+   hideColorById('I3');
+   showColorById('I3');
+}
+function sit36(elementId){
+   hideColorById('I4');
+   showColorById('I4');
+}
+function sit37(elementId){
+   hideColorById('J1');
+   showColorById('J1');
+}
+function sit38(elementId){
+   hideColorById('J2');
+   showColorById('J2');
+}
+function sit39(elementId){
+   hideColorById('J3');
+   showColorById('J3');
+}
+function sit40(elementId){
+   hideColorById('J4');
+   showColorById('J4');
+}
